7f0a16adc866281b47ecacd6a459d8fb7c0f3f79
unknown <derekp@ATGOLYDEV000141.atg.wa.lcl>
2015-02-20 11:38:30 -0800

    first push;currently only outputs list

---
+++ env-clean/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace env_clean
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var path = GetEnv();
+
+            foreach (var p in path)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        private static IEnumerable<string> GetEnv()
+        {
+            var env = Environment.GetEnvironmentVariable("Path");
+
+            return env != null ? env.Split(';') : new[] { "" };
+        }
+    }
+}
